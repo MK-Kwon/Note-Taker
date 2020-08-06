4d37d212f1fe1b1548bd2407ca940d7bb86cef1d
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-08-06 12:26:30 +0930

    Designed all functions to be built

---
+++ public/assets/js/index.js
@@ -0,0 +1,70 @@
+const $noteTitle = $(".note-title");
+const $noteText =$(".note-textarea");
+const $saveNoteBtn =$(".save-note");
+const $newNoteBtn =$(".new-note");
+const $noteList =$(".list-container .list-group");
+
+// activeNote is used to keep track of the note in the testarea
+const activeNote = {};
+
+// Function for getting all notes from the db
+const getNotes = function() {
+
+};
+
+// Function for saving a note to the db
+const saveNote = function() {
+
+};
+
+// Function for deleting a note from the db
+const deleteNote = function() {
+
+};
+
+
+// If there's an activeNote, display it. Otherwise render empty inputs
+const renderActiveNotes = function() {
+
+};
+
+
+// Function to get the note data from the inputs, save it to the db and update the view
+const handleNoteSave = function() {
+
+};
+
+// Function to delete the saved notes when clicked
+const handleNoteDelete = function() {
+
+};
+
+// Function to display saved notes on to the testarea when clicked
+const handleNoteView = function() {
+
+};
+
+
+// Function to remove unsaved new notes from textarea so that the user can write a new one
+const handleNewNoteView = function() {
+
+};
+
+
+// Function to hide the save button if a note's title or text are empty. Or else show the button
+const handleRenderSaveBtn = function() {
+
+};
+
+// Function to render the list of note titles in note-textarea
+const renderNoteList = function() {
+
+};
+
+// Function to get notes from the db and render them in note-textarea(when initialise the application)
+const getAndRenderNotes = function() {
+
+};
+
+// Gets and renders the list of saved notes when initialised
+getAndRenderNotes();
